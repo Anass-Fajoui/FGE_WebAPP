a92a12e647b89bf5bda7e7f14e942f7747806ef1
Anass Fajoui <anasslpro165@gmail.com>
2024-12-14 18:02:23 +0100

    Finish CRUD for the member

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from models import db, Membre
 
 app = Flask(__name__)
 
@@ -7,17 +8,12 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:anasslpro@localhost/fge'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
-# Define your model (assuming a table called Membre)
-class Membre(db.Model):
-    __tablename__ = 'Membre'
-    Membre_id = db.Column(db.Integer, primary_key=True)
-    Nom = db.Column(db.String(100))
-    Prenom = db.Column(db.String(100))
-    Email = db.Column(db.String(100))
-    Role = db.Column(db.String(50))
-    Club_id = db.Column(db.Integer)
+# Route to home page
+@app.route('/')
+def home():
+    return render_template('home.html')
 
 # Route to list all members
 @app.route('/members')
@@ -25,7 +21,7 @@ def list_members():
     members = Membre.query.all()  # Get all members from the database
     return render_template('members.html', members=members)
 
-
+# Route to add a new member
 @app.route('/add_member', methods=['GET', 'POST'])
 def add_member():
     if request.method == 'POST':
@@ -45,6 +41,31 @@ def add_member():
         return redirect('/members')  # Redirect to the members list
     
     return render_template('add_member.html')
+
+# Route to delete a member
+@app.route('/edit_member/<int:id>', methods=['GET', 'POST'])
+def edit_member(id):
+    member = Membre.query.get(id)
+    
+    if request.method == 'POST':
+        member.Nom = request.form['Nom']
+        member.Prenom = request.form['Prenom']
+        member.Email = request.form['Email']
+        member.Role = request.form['Role']
+        member.Club_id = request.form['Club_id']
+        
+        db.session.commit()
+        return redirect('/members')  # Redirect to the members list
+    
+    return render_template('edit_member.html', member=member)
+
+@app.route('/delete_member/<int:id>', methods=['GET', 'POST'])
+def delete_member(id):
+    member = Membre.query.get(id)
+    db.session.delete(member)
+    db.session.commit()
+    return redirect('/members')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
